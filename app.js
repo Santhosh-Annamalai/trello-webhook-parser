2b0541a11ecd6772416a3af2d062a3746cfb02b9
Santhosh-Annamalai <santhoshannamalai89@outlook.com>
2018-07-10 18:12:41 +0530

    Some more changes

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const superagent = require("superagent");
 const app = express();
+const discordWebhookURL = require("./config.js").discordWebhookURL;
+
+/*eslint-disable no-console*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", (req) => {
     superagent
-    .post("https://discordapp.com/api/webhooks/465856126244880388/QIynzGFp2myQ0KAAYIE_Ump0uWFVFCEPDa16HMqB60S2hm3RHvjlZlzexeDPU0wWlAM5")
+    .post(`${discordWebhookURL}`)
     .field({
         content: req.body
     })
     .then(value => console.log(value.text))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err.text));
 });
 
 app.listen(80, console.log("Listening"));
