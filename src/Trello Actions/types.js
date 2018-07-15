d3fc5902a4dfa6828691683106d5896066817ae7
Santhosh-Annamalai <santhoshannamalai89@outlook.com>
2018-07-15 21:45:47 +0530

    Added additional types

--- src/Trello Actions/types.js
+++ src/Trello Actions/types.js
@@ -70,5 +70,35 @@ module.exports.typeParser = function actionType(data, triggered_by) {
             return verboseData = `A label named ${data.data.label.name} of color ${data.data.label.color} has been removed from the card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink})`;
         }
     }
+    if (type === "deleteComment") {
+        return verboseData = `${triggered_by} deleted their comment on card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink})`;
+    }
+    if (type === "updateComment") {
+        return verboseData = `${triggered_by} updated their comment on card [${data.data.card.name}](https://trello.com/c/${data.data.card.shortLink}/${data.data.card.idShort}-${data.data.card.name}#comment-${data.id})\n\n**Old Content**:\n\`\`\`${data.data.old.text}\n\n**New Content**:\n\`\`\`${data.data.action.text}\`\`\``;
+    }
+    if (type === "addCheckListToCard") {
+        return verboseData = `A checklist named ${data.data.checklist.name} has been added to the card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink})`;
+    }
+    if (type === "createCheckItem") {
+        return verboseData = `An item named ${data.data.checkItem.name} has been added to the checklist ${data.data.checklist.name} of card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink})`;
+    }
+    if (type === "updateCheckItemStateOnCard") {
+        if (data.data.checkItem.state === "complete") {
+            return verboseData = `An item named ${data.data.checkItem.name} of card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink}) has been marked as \`completed\``;
+        }
+        else {
+            return verboseData = `An item named ${data.data.checkItem.name} of card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink}) has been changed back to \`incomplete\` state`;
+        }
+    }
+    if (type === "updateCheckItem") {
+        let verboseDataInitial = `An item name of checklist ${data.data.checklist.name} of card [${data.data.card.name}](https://www.trello.com/c/${data.data.card.shortLink}) has been updated from ${data.data.old.name} to ${data.data.checkItem.name}\n\n**Users mentioned**:\n$|8`;
+        let verboseDataArray = verboseDataInitial.split("$|8");
+        let rawHtmlData = data.display.entities.checkitem.nameHtml;
+        let parsedHtmlData = rawHtmlData.match(/(title=)(.*?)>/g).join(",").match(/"(.*?)"/g).join(",").match(/[^"]/g).join("").split(",");
+        for (let i = 0; i < parsedHtmlData.length; i++) {
+            verboseDataArray.push(`${i + 1}) [${parsedHtmlData[i]}](https://trello.com/${parsedHtmlData[i]})\n`);
+        }
+        return verboseData = verboseDataArray.join("");
+    }
     return verboseData;
 };
